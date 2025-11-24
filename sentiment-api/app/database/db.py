79a789bf90177ceb86fd5128a8695cd7c4895d9e
KarimaChami <karimachami24@gmail.com>
2025-11-24 12:20:55 +0100

    fixe docker file

--- sentiment-api/app/database/db.py
+++ sentiment-api/app/database/db.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.database.config import settings
-
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
